f9be04e999ed2597b6159215ced3bc95e200e720
Pham Gi <trungdunght2003@gmail.com>
2023-06-09 14:06:51 +0700

    ListView simple

--- app/src/main/java/com/example/example_dn/MainActivity.java
+++ app/src/main/java/com/example/example_dn/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.example_dn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list_view);
+        List<String> items = Arrays.asList("sup1", "sup2", "sup3");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(arrayAdapter);
+
     }
 }
